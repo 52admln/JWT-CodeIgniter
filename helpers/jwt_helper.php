30abf6f6d2028c4d7b8ea00d477df2e53e97b8e4
wyj <ismrwang@gmail.com>
2017-12-20 13:02:27 +0800

    fix token expired validation

--- helpers/jwt_helper.php
+++ helpers/jwt_helper.php
@@ -4,7 +4,7 @@ class jwt_helper extends CI_Controller
 {
     const CONSUMER_KEY = 'YOUR_KEY'; // please replace YOUR_XX
     const CONSUMER_SECRET = 'YOUR_SECRET'; // please replace YOUR_XX
-    const CONSUMER_TTL = 86400;
+    const CONSUMER_TTL = 86400; // second | 1 day
 
     // create token
     public static function create($userid)
@@ -26,8 +26,15 @@ class jwt_helper extends CI_Controller
         $CI =& get_instance();
         $CI->load->library('JWT');
         try {
-            $CI->jwt->decode($token, self::CONSUMER_SECRET);
-            return true;
+            $decodeToken = $CI->jwt->decode($token, self::CONSUMER_SECRET);
+            // validate token is not expired
+            $ttl_time = strtotime($decodeToken->issuedAt);
+            $now_time = strtotime(date(DATE_ISO8601, strtotime("now")));
+            if(($now_time - $ttl_time) > $decodeToken->ttl) {
+                throw new Exception('Expired');
+            } else {
+                return true;
+            }
         } catch (Exception $e) {
             return false;
         }
